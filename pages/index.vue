2b88d1559bada94ee0451fea8386cabeab536fe2
kiriha1203 <mizumoto.takefumi@gmail.com>
2021-06-15 00:25:16 +0900

    Vuex classic → module

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- <p>{{ $store.state.message }}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- <button @click="$store.commit('updateMessage')">Update</button> -->
+      <!-- <button @click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button> -->
+      <button @click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')">Dispatch</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
